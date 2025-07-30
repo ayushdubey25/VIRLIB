00eb0bd63cf67e6585b0639dcc9f43f04586c4f2
Ayush Dubey <154453637+ayushdubey25@users.noreply.github.com>
2025-07-30 20:33:21 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,7 +12,11 @@ import commentsRoutes from "./routes/comments.js";
 
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "https://virlib-frontend.onrender.com",
+  credentials: true,
+}));
+
 
 // ⬇️ Increase payload size limit to 50MB
 app.use(express.json({ limit: "50mb" }));
